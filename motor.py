8f21056772662a3531ee025ae905b71dabda8c1d
Sathira Rumal <sathira@chiefdx.com>
2025-04-24 14:08:14 +0530

    done

--- motor.py
+++ motor.py
@@ -1,9 +1,12 @@
 import RPi.GPIO as GPIO
 import time
 
+GPIO.cleanup()
 # ============== SETUP GPIO =============
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(18, GPIO.OUT, initial=GPIO.LOW)
+
+GPIO.setup(18, GPIO.OUT)
+GPIO.output(18, GPIO.LOW)
 
 while True:
     GPIO.output(18, GPIO.HIGH)
